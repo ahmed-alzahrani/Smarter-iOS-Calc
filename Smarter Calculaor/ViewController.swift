cbbc3155d33ea5df92e38b7441b8b3be4d27aba2
ahmed-alzahrani <ahmedalzahrani94@gmail.com>
2018-05-07 13:13:28 -0700

    Initial Commit

---
+++ Smarter Calculaor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smarter Calculaor
+//
+//  Created by Ahmed Al-Zahrani on 2018-05-07.
+//  Copyright © 2018 Ahmed Al-Zahrani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
